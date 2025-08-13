9b447382040c001a22e6d49463a24a148ba0f3dd
Phil Hawksworth <phil@deno.com>
2025-08-13 13:26:38 +0100

    try text span

--- partials/og.ts
+++ partials/og.ts
@@ -1,19 +1,15 @@
 // import satori from "satori/wasm";
 import { Resvg, ResvgRenderOptions } from "@resvg/resvg-js";
 
-export default (imageUrl: string, color: string, url: string) => {
+export default (imageUrl: string, color: string) => {
 
   if (!color || color === "undefined") {
     color = "000000";
   }
-
+  
   const svg = `
   <svg width="1280" height="720" viewBox="0 0 1280 720" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
-      <style type="text/css">
-
-  
-      </style>
     </defs>
     <rect fill="#${color}" x="965" y="0" width="315" height="720"></rect>
     <image href="./public/images/og-bg.svg" x="0" y="0" width="1280" height="720" />
@@ -21,15 +17,22 @@ export default (imageUrl: string, color: string, url: string) => {
     <g transform="translate(724,104)">
       <path fill="#${color}" d="M217 31c2 2 2 4 0 6l-28 29a5 5 0 0 1-7-6l26-26-26-25a4 4 0 1 1 6-7l29 29ZM5 241H1C13 136 66 83 117 56a238 238 0 0 197-26 268 268 0 0 1 0 9 58 58 0 0 0-8 0l-20 3c-18 3-41 9-65 22-48 25-99 76-111 178l-5-1Z"/>
     </g>
-    <text x="1120" y="200" text-anchor="middle" fill="#ffffff" font-size="40" font-weight="bold">#${color.toUpperCase()}</text>
-    <text x="1120" y="250" text-anchor="middle" fill="#ffffff" font-size="40" font-weight="bold">TEST TEXT</text>
-  </svg>
-  `;
+    <text fill="#ffffff" font-family="" font-size="40" text-anchor="middle">
+      <tspan x="1120" y="200">#${color.toUpperCase()}</tspan>
+    </text>
+    </svg>
+    `;
+
 
   const opts = {
     fitTo: {
       mode: "width",
       value: 1280,
+    },
+    font: {
+      fontFiles: ['./public/fonts/Moranga-Medium.woff2"'], // Load custom fonts.
+      loadSystemFonts: true, // It will be faster to disable loading system fonts.
+      // defaultFontFamily: '', // You can omit this.
     },
   };
   const resvg = new Resvg(svg, opts as ResvgRenderOptions);
