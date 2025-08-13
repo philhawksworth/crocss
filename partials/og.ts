aa294324a31b1f12a869331aca49df002b770532
Phil Hawksworth <phil@deno.com>
2025-08-13 14:01:55 +0100

    take out the dynamic text form the OG

--- partials/og.ts
+++ partials/og.ts
@@ -9,17 +9,12 @@ export default (imageUrl: string, color: string) => {
   
   const svg = `
   <svg width="1280" height="720" viewBox="0 0 1280 720" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <defs>
-    </defs>
     <rect fill="#${color}" x="965" y="0" width="315" height="720"></rect>
     <image href="./public/images/og-bg.svg" x="0" y="0" width="1280" height="720" />
     <image href="${imageUrl}" x="230" y="-50" width="500" />
     <g transform="translate(724,104)">
       <path fill="#${color}" d="M217 31c2 2 2 4 0 6l-28 29a5 5 0 0 1-7-6l26-26-26-25a4 4 0 1 1 6-7l29 29ZM5 241H1C13 136 66 83 117 56a238 238 0 0 197-26 268 268 0 0 1 0 9 58 58 0 0 0-8 0l-20 3c-18 3-41 9-65 22-48 25-99 76-111 178l-5-1Z"/>
     </g>
-    <text fill="#ffffff" font-family="" font-size="40" text-anchor="middle">
-      <tspan x="1120" y="200">#${color.toUpperCase()}</tspan>
-    </text>
     </svg>
     `;
 
@@ -28,14 +23,7 @@ export default (imageUrl: string, color: string) => {
     fitTo: {
       mode: "width",
       value: 1280,
-    },
-    font: {
-      loadSystemFonts: true, // It will be faster to disable loading system fonts.
-      fontFiles: [`${Deno.cwd()}/public/fonts/Moranga-Medium.woff2`],
-      // fontDirs: ['./public/fonts'],
-      defaultFontFamily: 'serif',
-    },
-    logLevel: 'debug', // Default Value: error
+    }
   };
   const resvg = new Resvg(svg, opts as ResvgRenderOptions);
   const pngData = resvg.render();
